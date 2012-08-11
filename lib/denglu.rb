eb95b22149ccd04cfee4e8608ded3c358128b6b6
Spring MC <Heresy.Mc@gmail.com>
2012-08-11 17:24:29 +0800

    add auth module

--- lib/denglu.rb
+++ lib/denglu.rb
@@ -2,6 +2,9 @@
 require 'json'
 require 'open-uri'
 require 'rest_client'
+require 'denglu/base'
+require 'denglu/auth'
+require 'denglu/comment'
 require 'denglu/version'
 
 module Denglu
---
+++ lib/denglu/auth.rb
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+module Denglu
+
+  class Auth
+    #TODO
+  end
+
+end
