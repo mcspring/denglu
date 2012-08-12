67fc0a0011c626e1057d63333d393be3742c34a9
Spring MC <Heresy.Mc@gmail.com>
2012-08-12 09:27:11 +0800

    update comment specs :heart:

--- spec/denglu/comment_spec.rb
+++ spec/denglu/comment_spec.rb
@@ -7,27 +7,47 @@ module Denglu
       @comment = Comment.new
     end
 
-    context '.list' do
+    context ".list" do
       it "should works" do
         result = @comment.list
-
         result.should be_instance_of(Array)
+        result.to_json.should include('parentID')
       end
     end
 
-    context '.latest' do
+    context ".latest" do
       it "should works" do
         result = @comment.latest
-
         result.should be_instance_of(Array)
+        result.to_json.should_not include('parentID')
       end
     end
 
-    context '.total' do
+    context ".total" do
       it "should works" do
         result = @comment.total
 
         result.should be_instance_of(Array)
+      end
+
+      context "with argument" do
+        before :each do
+          @resource = @comment.total[0]
+        end
+
+        it "should return a hash object when supply resource id" do
+          resource_id = @resource['id'].to_i
+
+          result = @comment.total(resource_id)
+          result.should be_instance_of(Hash)
+        end
+
+        it "should return a hash object when supply resource url" do
+          resource_url = @resource['url']
+
+          result = @comment.total(resource_url)
+          result.should be_instance_of(Hash)
+        end
       end
     end
   end
